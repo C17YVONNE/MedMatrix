52bd631d05b3a486ecd21ecfcec84848fca90db3
C17YVONNE <c17yvonne@gmail.com>
2024-09-03 22:45:30 +0900

    登録したユーザーのセッションによって登録者情報の表示  ・URLのフィルターの追加

--- HomeController.java
+++ HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.emrsys.medmatrix.util.Session;
+import com.emrsys.medmatrix.util.UserInfoSession;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -13,15 +13,15 @@ public class HomeController {
 
 	@GetMapping("/home")
 	public String home(HttpSession httpSession, Model model) {
-		Session session = (Session) httpSession.getAttribute("session");
+		UserInfoSession userInfoSession = (UserInfoSession) httpSession.getAttribute("session");
 
 		// 检查Session对象是否存在，防止直接访问 /home 页面而没有登录
-		if (session == null) {
+		if (userInfoSession == null) {
 			// 如果没有Session对象，重定向到登录页面
 			return "redirect:/login";
 		}
-		model.addAttribute("session", session); 
-		System.out.println(session);
+		model.addAttribute("userInfoSession", userInfoSession); 
+		System.out.println(userInfoSession);
 		System.out.println("/n");
 		return "home";
 	}
--- UserLoginController.java
+++ UserLoginController.java
@@ -5,15 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.emrsys.medmatrix.entity.UserLoginEntity;
 import com.emrsys.medmatrix.object.UserLoginDto;
 import com.emrsys.medmatrix.service.UserLoginService;
 import com.emrsys.medmatrix.util.MsgContents;
-import com.emrsys.medmatrix.util.Session;
 import com.emrsys.medmatrix.util.Url;
+import com.emrsys.medmatrix.util.UserInfoSession;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -50,27 +49,12 @@ public class UserLoginController {
 		// 验证密码是否匹配
 		if (userLoginService.checkPassword(rawPassword, storedEnncodedPassword)) {
 			// 如果匹配，设置登录会话信息
-			Session session = userLoginService.getLoginSession(doctorId);
-			httpSession.setAttribute("session", session);// 将Session对象放入HttpSession中
-			System.out.println(session);
-			System.out.println("/n");
+			UserInfoSession userInfoSession = userLoginService.getLoginSession(doctorId);
+			httpSession.setAttribute("session", userInfoSession);// 将Session对象放入HttpSession中
 			return "redirect:/home";
 		} else {
 			model.addAttribute("error", MsgContents.LOGINFAILUER);
 			return "login";
 		}
-	}
-
-	/**
-	 * ログアウト 
-	 * 
-	 */
-	@RequestMapping(Url.LOGINOUT)
-	public String loginout(HttpSession session) {
-
-		session.removeAttribute("userLogin");
-
-		return "redirect:/home";
-
 	}
 }
